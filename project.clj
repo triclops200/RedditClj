740b1875f2c5f88e36ce63408fc7261455ede26b
Bryan Hoyle <starfoxprime@gmail.com>
2014-01-16 11:54:06 -0500

    Reddit project rename

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject reddit "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.4"]
+                 [clj-http "0.7.8"]
+                 [clj-time "0.6.0"]]
+  :main ^:skip-aot reddit.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
